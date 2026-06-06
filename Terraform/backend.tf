59546d577e672bd437433a001e43fce189cb810a
ShahidKhan232 <shahidkhan.95173@gmail.com>
2026-06-06 13:22:46 +0530

    initial commit

---
+++ Terraform/backend.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = ">= 1.13.3, < 1.15.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.14.1"
+    }
+  }
+}
+provider "aws" {
+  region = var.aws-region
+}
